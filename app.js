5ccc847fcd4ef7b770ee8617b6e07af4d8911f1f
Tr0n321 <pharrington2@yahoo.com>
2026-07-12 18:20:11 -0400

    Completed Module 2 MVC conversion

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -19,10 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
+app.use('/', indexRouter);
+app.use('/travel', travelRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
